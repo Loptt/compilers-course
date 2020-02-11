47aa546c58e925da4bb80828758a02d94df7aafc
Carlos Estrada <carlos.deg02@hotmail.com>
2020-02-11 10:01:53 -0600

    Added hash table

--- hw1/hash_table.go
+++ hw1/hash_table.go
@@ -1,36 +1,80 @@
 package main
 
+// TableEntry specifies a field in the hashtable
 type TableEntry struct {
 	key   int
 	value string
 	next  *TableEntry
 }
 
+// SIZE of the HashTable array implementation
+const SIZE = 10
+
+// HashTable implemenets a hash table data structure
 type HashTable struct {
-	array [100]*TableEntry
+	array [SIZE]*TableEntry
 }
 
+// Insert adds an element to the table
 func (h *HashTable) Insert(key int, value string) {
-	index := key % 100
+	index := key % SIZE
 
 	if h.array[index] != nil {
 		curr := h.array[index]
 
 		for curr.next != nil {
+			if curr.key == key {
+				return
+			}
 			curr = curr.next
 		}
+
+		if curr.key == key {
+			return
+		}
+
 		curr.next = &TableEntry{key, value, nil}
 	} else {
 		h.array[index] = &TableEntry{key, value, nil}
 	}
 }
 
+// Get retreives the value corresponding to the key
 func (h *HashTable) Get(key int) string {
-	index := key % 100
+	index := key % SIZE
 
-	if h.array[index] == nil {
-		return ""
-	} else {
+	if h.array[index] != nil {
+		curr := h.array[index]
 
+		for curr != nil {
+			if curr.key == key {
+				return curr.value
+			}
+			curr = curr.next
+		}
+	}
+
+	return ""
+}
+
+// Remove deletes the TableEntry corresponding to the key
+func (h *HashTable) Remove(key int) {
+	index := key % SIZE
+
+	if h.array[index] != nil {
+		curr := h.array[index]
+
+		if curr.key == key {
+			h.array[index] = curr.next
+			return
+		}
+
+		for curr.next != nil {
+			if curr.next.key == key {
+				curr.next = curr.next.next
+				return
+			}
+			curr = curr.next
+		}
 	}
 }
--- hw1/main.go
+++ hw1/main.go
@@ -3,21 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	var q Queue
-	q.Push(1)
-	q.Push(3)
-	q.Push(2)
-	fmt.Println(q.Front())
-	q.Pop()
-	fmt.Println(q.Front())
-	q.Push(5)
-	fmt.Println(q.Front())
-	q.Pop()
-	fmt.Println(q.Front())
-	fmt.Println("All queue")
-
-	for !q.Empty() {
+	// Queue
+	/*
+		var q Queue
+		q.Push(1)
+		q.Push(3)
+		q.Push(2)
 		fmt.Println(q.Front())
 		q.Pop()
-	}
+		fmt.Println(q.Front())
+		q.Push(5)
+		fmt.Println(q.Front())
+		q.Pop()
+		fmt.Println(q.Front())
+		fmt.Println("All queue")
+
+		for !q.Empty() {
+			fmt.Println(q.Front())
+			q.Pop()
+		}
+	*/
+
+	// HashTable
+	var ht HashTable
+	ht.Insert(4, "Hola")
+	ht.Insert(14, "como")
+	ht.Insert(34, "estas")
+	ht.Insert(44, "bien")
+	fmt.Println(ht.Get(4))
+	fmt.Println(ht.Get(14))
+	fmt.Println(ht.Get(34))
+	fmt.Println(ht.Get(44))
+	ht.Remove(4)
+	fmt.Println(ht.Get(4))
+
 }
